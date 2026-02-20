3be417da3701ac0f2294f8104d6de1e251dfb2da
JoaoHSL-11 <limajoahenrique@uol.com.br>
2026-02-20 14:20:45 -0300

    chore: remove unrelated project directories

--- Python/maximaze.py
+++
@@ -1,8 +0,0 @@
-def resolver():
-    casos_de_teste = int(input())
-    for _ in range(casos_de_teste):
-        x = int(input())
-        y = x - 1
-        print(y)
-
-resolver()
--- Python/maximum.py
+++
@@ -1,6 +0,0 @@
-def solucao():
-    casos  = int(input())
-    for _ in range(casos):
-        n = int(input())
-        print(n//2)
-solucao()
